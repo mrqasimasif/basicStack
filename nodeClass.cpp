1696578da35211c164c881575a11380bff4a2f90
Qasim Asif <meqasimasif@gmail.com>
2019-09-29 12:20:30 +0500

    Added operational nodeClass.cpp

--- nodeClass.cpp
+++ nodeClass.cpp
@@ -1,27 +1,33 @@
 /*
-Self Referential Node Class decleration and definition that are going to serve as our STACK elements.
+Self Referential Node Class decleration and definition that are going to serve as our STACK elements
 */
 #include<iostream>
 using namespace std;
 
-class cNode{
-    //Data carrier of cNode objetct
-    int data;
+class cNode {
+	//Data carrier of cNode objetct and operator carrier
+	int data;
+	char opr;
 public:
-    //Reference to next cNode Object
-    cNode* nextNode;
+	//Reference to next cNode Object
+	cNode* nextNode;
 
-    //Using this pointer to refer to the same class object
-    //Basic Class Constructors
-    cNode() { this->data = 0; }
-    cNode( int number ) { this->data = number; }
+	//Using this pointer to refer to the same class object
+	//Basic Class Constructors / Overlaoded constructors
+	cNode() { this->data = -1; this->opr = '~'; }
+	cNode(int number) { this->data = number; this->opr = '~'; }
+	cNode(char oper) { this->opr = oper; this->data = -1; }
+	cNode(int number, char oper) { this->data = number; this->opr = oper; }
 
-    //Class Getter
-    int getData() { return data; }
+	//Class Getter
+	int getData() { return data; }
+	char getOpr() { return opr; }
 
-    //Class Setter
-    void setData( int number ) { this->data = number; }
+	//Class Setter
+	void setData(int number, char oper) { this->data = number; this->opr = oper; }
 
-    //Printing the value of data carrier
-    void printData() { cout<<data; }
+	//Printing the value of data carrier
+	void printData() { cout << data << "\t" << opr;	}
+
 };
+#pragma once
