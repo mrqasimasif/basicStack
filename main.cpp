232722a8122a051d8fa8fac3fb6e104ae34041fc
Qasim Asif <meqasimasif@gmail.com>
2019-09-29 12:20:57 +0500

    Added operationl main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /*
 The driver program to check everything defined in other heads
-Pushing and Poping out nodes from the stack 
 */
 
 #include<iostream>
@@ -8,31 +7,85 @@ Pushing and Poping out nodes from the stack
 #include"stack.cpp"
 
 using namespace std;
+int performOper(char, int*);
 
 int main() {
+	int result = 0;
 	//Defining first STACK object 
-	cStack stk;
-	//Defining NODE objects to perform functions of push and pop in STACK
-	cNode *nd1, *nd2, *nd3;
+	cStack inputStack, aidStack;
 
-	cout << "Prinitng empty Stack\n";
-	stk.printStack();
-	cout << "Pushing Reference to dynamic elements in Stack\n";
+	//Defining NODE objects to perform functions of push and pop in STACK ,to furthur continue to expression evaluation
+	cNode *n[9];
+	n[0] = new cNode(2);
+	n[1] = new cNode(3);
+	n[2] = new cNode(5);
+	n[3] = new cNode('/');
+	n[4] = new cNode('-');
+	n[5] = new cNode(2);
+	n[6] = new cNode(6);
+	n[7] = new cNode('+');
+	n[8] = new cNode('*');
+
+	inputStack.push(n[0]);
+	inputStack.push(n[1]);
+	inputStack.push(n[2]);
+	inputStack.push(n[3]);
+	inputStack.push(n[4]);
+	inputStack.push(n[5]);
+	inputStack.push(n[6]);
+	inputStack.push(n[7]);
+	inputStack.push(n[8]);
 	
-	nd1 = new cNode(18);
-	nd2 = new cNode(20);
-	nd3 = new cNode(30);
+	cout << "Alaala";
+	while (inputStack.isNotEmpty()) {
+		cNode *instantPop = inputStack.pop();
+		if (instantPop->getOpr() == '~') {
+			cout << "blabla";
+			//Yeah masla kray ga
+			aidStack.push(instantPop);
+			cout << "\nPrinting aidStack" << endl;
+			aidStack.printStack();
+		}
+		else {
+			cout << "Clacla";
+			/*int i = 0;
+			int op[2],tempResult;
+			while (i < 2) {
+				cout << "Dladla";
+				cNode *temNode;
+				cout << "Dladla1";
+				temNode = aidStack.pop();
+				cout << "Dladla2";
+				op[i] = aidStack.pop()->getData();
+				cout << "Dladla3";
+				i++;
+				cout << "Dladla4";
+			}
+			cout << "Elaela";
+			char opera = instantPop->getOpr();
+			tempResult = performOper(opera, op);
+			cNode *tempNode = new cNode(result);
+			aidStack.push(tempNode);*/
+		}
+	}
 
-	//Pusing 2 nodes to s tack
-	stk.push(nd1);
-	stk.push(nd2);
-	stk.printStack();
-
-    //Pushing 3rd node to stack
-	stk.push(nd3);
-	stk.printStack();
+	cout << "The Result of the postFix expression is : ";
+	aidStack.printStack();
 
 	system("pause");
-	
 	return 0;
+}
+
+
+int performOper(char opr, int *num) {
+	if (opr == '+') 
+		return *num + *(num + 1);
+	else if (opr == '-') 
+		return *num - *(num + 1);
+	else if (opr == '*') 
+		return *num * *(num + 1);
+	else if (opr == '/')
+		return *num / *(num + 1);
+	else 
+		return *num % *(num + 1);
 }
