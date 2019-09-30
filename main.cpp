cca4fa9425a8bb8596bb2c4787de46f0cd7d27f0
Qasim Asif <meqasimasif@gmail.com>
2019-09-30 20:30:38 +0500

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,79 +7,98 @@ The driver program to check everything defined in other heads
 #include"stack.cpp"
 
 using namespace std;
-int performOper(char, int*);
+int performOper(char, int, int);
 
 int main() {
-	int result = 0;
+	//int result = 0;
 	//Defining first STACK object 
 	cStack inputStack, aidStack;
 
 	//Defining NODE objects to perform functions of push and pop in STACK ,to furthur continue to expression evaluation
+	//To solve a postfix expression written in reverse order 
+	//least significant operator or number in the first node
 	cNode *n[9];
-	n[0] = new cNode(2);
-	n[1] = new cNode(3);
-	n[2] = new cNode(5);
-	n[3] = new cNode('/');
-	n[4] = new cNode('-');
+	n[0] = new cNode('+');
+	n[1] = new cNode(6);
+	n[2] = new cNode('-');
+	n[3] = new cNode('*');
+	n[4] = new cNode(3);
 	n[5] = new cNode(2);
-	n[6] = new cNode(6);
-	n[7] = new cNode('+');
-	n[8] = new cNode('*');
+	n[6] = new cNode('+');
+	n[7] = new cNode(3);
+	n[8] = new cNode(5);
 
+	//Pushing all nodes into input Stack used in operation
 	for (int i = 0; i < 9; i++) {
 		inputStack.push(n[i]);
 	}
-		
-	cout << "Alaala";
+
+	//while loop that allow to run till the end of stack
 	while (inputStack.isNotEmpty()) {
+
+		//poping input stack element one by one
 		cNode *instantPop = inputStack.pop();
+	
+		//to check for the first operator comes in stack ( ~ denotes no operator)
 		if (instantPop->getOpr() == '~') {
-			cout << "blabla";
-			//Yeah masla kray ga
+			//using aid stack for operation solving
 			aidStack.push(instantPop);
-			cout << "\nPrinting aidStack" << endl;
-			aidStack.printStack();
 		}
 		else {
-			cout << "Clacla";
-			/*int i = 0;
-			int op[2],tempResult;
-			while (i < 2) {
-				cout << "Dladla";
-				cNode *temNode;
-				cout << "Dladla1";
-				temNode = aidStack.pop();
-				cout << "Dladla2";
-				op[i] = aidStack.pop()->getData();
-				cout << "Dladla3";
-				i++;
-				cout << "Dladla4";
-			}
-			cout << "Elaela";
-			char opera = instantPop->getOpr();
-			tempResult = performOper(opera, op);
-			cNode *tempNode = new cNode(result);
-			aidStack.push(tempNode);*/
+			//vaiables for storing the operators , operands and the result of operation
+			int op1 = 0, op2 = 0, tempResult;
+			char opera;
+
+			//two node pointers for storing the dynamic nodes invloved in operation
+			cNode *temNode1,*temNode2;
+			temNode1 = aidStack.pop();
+			temNode2 = aidStack.pop();
+			
+			op1= temNode1->getData();
+			op2 = temNode2->getData();
+
+			//Operator is from the current poped node that is instantPop
+			opera = instantPop->getOpr();
+
+			//perdorming operation on operands
+			tempResult = performOper(opera, op1,op2);
+
+			//initializing new node with the result to be again stored in aid stack and pushing 
+			cNode *tempNode = new cNode(tempResult);
+			aidStack.push(tempNode);
+
+			//clearing the dynamic memory
+			delete temNode1, temNode2, tempNode;
 		}
 	}
 
-	cout << "The Result of the postFix expression is : ";
+	cout << "\nThe Result of the postFix expression is :";
 	aidStack.printStack();
 
-	system("pause");
+	system ("pause");
 	return 0;
 }
 
-
-int performOper(char opr, int *num) {
+// function to perform operation on operands
+int performOper(char opr, int num1, int num2) {
 	if (opr == '+') 
-		return *num + *(num + 1);
-	else if (opr == '-') 
-		return *num - *(num + 1);
+		return (num1 + num2);
+		
+	else if (opr == '-'){
+		if (num1 > num2)
+			return num1 - num2;
+		else
+			return num2 - num1;
+	}
 	else if (opr == '*') 
-		return *num * *(num + 1);
-	else if (opr == '/')
-		return *num / *(num + 1);
+		return (num1 * num2);
+
+	else if (opr == '/'){
+		if (num1 > num2)
+			return (num1 / num2);
+		else
+			return (num2 / num1);
+	}
 	else 
-		return *num % *(num + 1);
+		return (num1 % num2);
 }
